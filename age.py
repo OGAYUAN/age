886bf914e6c06aeab3cf2a757582f84b3fb2d1e1
unknown <scyuan4@gmail.com>
2018-11-18 14:27:31 +0800

    usa_driving

--- age.py
+++ age.py
@@ -1,9 +1,14 @@
 #年齡判斷程式
-age = input('請輸入年齡: ')
 country = input('請問你的國籍是: ')
+age = input('請輸入年齡: ')
 age = int(age)
 if country == '台灣':
 	if age >= 18:
 		print('你可以考駕照')
 	else:
 		print('你還不能考駕照')
+elif country == '美國':
+    if age >= 16:
+        print('你可以考駕照')   
+    else:
+        print('你還不能考駕照')
